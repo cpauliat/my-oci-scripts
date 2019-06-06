498b2b134a063c327ac2543f70d3272ed7a4bee4
Christophe Pauliat <christophe.pauliat@oracle.com>
2019-06-06 16:12:47 +0200

    fix

--- OCI_objects_list_in_compartment.sh
+++ OCI_objects_list_in_compartment.sh
@@ -64,13 +64,15 @@ EOF
 COLORED_OUTPUT=true
 if [ "$COLORED_OUTPUT" == true ]
 then
-  COLOR_TITLE="\033[32m"              # green
+  COLOR_TITLE1="\033[91m"             # green
+  COLOR_TITLE2="\033[32m"             # green
   COLOR_AD="\033[94m"                 # light blue
   COLOR_COMP="\033[93m"               # light yellow
   COLOR_BREAK="\033[91m"              # light red
   COLOR_NORMAL="\033[39m"
 else
-  COLOR_TITLE=""
+  COLOR_TITLE1=""
+  COLOR_TITLE2=""
   COLOR_AD=""
   COLOR_COMP=""
   COLOR_BREAK=""
@@ -81,248 +83,198 @@ fi
 list_compute_instances()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== COMPUTE: Instances${COLOR_NORMAL}"
-  echo
-  oci --profile $lp compute instance list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== COMPUTE: Instances${COLOR_NORMAL}"
+  oci --profile $lp compute instance list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_compute_custom_images()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== COMPUTE: Custom Images${COLOR_NORMAL}"
-  echo
-  oci --profile $lp compute image list -c $COMPID --output table --all --query "data [?\"compartment-id\"!=null].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}" 2>/dev/null
+  echo -e "${COLOR_TITLE2}========== COMPUTE: Custom Images${COLOR_NORMAL}"
+  oci --profile $lp compute image list -c $COMPID --output table --all --query 'data [?"compartment-id"!=null].{Name:"display-name", OCID:id, Status:"lifecycle-state"}' 2>/dev/null
   # 2>/dev/null needed to remove message "Query returned empty result, no output to show."
 }
 
 list_compute_boot_volumes()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== COMPUTE: Boot volumes${COLOR_NORMAL}"
-  echo
+  echo -e "${COLOR_TITLE2}========== COMPUTE: Boot volumes${COLOR_NORMAL}"
   for ad in $ADS
   do
-    echo -e "${COLOR_AD}Availability-domain $ad${COLOR_NORMAL}"
-    oci --profile $lp bv boot-volume list -c $COMPID --output table --all --availability-domain $ad --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+    echo -e "${COLOR_AD}== Availability-domain $ad${COLOR_NORMAL}"
+    oci --profile $lp bv boot-volume list -c $COMPID --output table --all --availability-domain $ad --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
   done
 }
 
 list_compute_boot_volume_backups()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== COMPUTE: Boot volume backups${COLOR_NORMAL}"
-  echo
-  oci --profile $lp bv boot-volume-backup list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== COMPUTE: Boot volume backups${COLOR_NORMAL}"
+  oci --profile $lp bv boot-volume-backup list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_block_storage_volumes()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== BLOCK STORAGE: Block volumes${COLOR_NORMAL}"
-  echo
+  echo -e "${COLOR_TITLE2}========== BLOCK STORAGE: Block volumes${COLOR_NORMAL}"
   for ad in $ADS
   do
-    echo -e "${COLOR_AD}Availability-domain $ad${COLOR_NORMAL}"
-    oci --profile $lp bv volume list -c $COMPID --output table --all --availability-domain $ad --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+    echo -e "${COLOR_AD}== Availability-domain $ad${COLOR_NORMAL}"
+    oci --profile $lp bv volume list -c $COMPID --output table --all --availability-domain $ad --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
   done
 }
 
 list_block_storage_volume_backups()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== BLOCK STORAGE: Block volume backups${COLOR_NORMAL}"
-  echo
-  oci --profile $lp bv backup list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== BLOCK STORAGE: Block volume backups${COLOR_NORMAL}"
+  oci --profile $lp bv backup list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_block_storage_volume_groups()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== BLOCK STORAGE: Volumes groups${COLOR_NORMAL}"
-  echo
+  echo -e "${COLOR_TITLE2}========== BLOCK STORAGE: Volumes groups${COLOR_NORMAL}"
   for ad in $ADS
   do
-    echo -e "${COLOR_AD}Availability-domain $ad${COLOR_NORMAL}"
-    oci --profile $lp bv volume-group list -c $COMPID --output table --all --availability-domain $ad --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+    echo -e "${COLOR_AD}== Availability-domain $ad${COLOR_NORMAL}"
+    oci --profile $lp bv volume-group list -c $COMPID --output table --all --availability-domain $ad --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
   done
 }
 
 list_block_storage_volume_group_backups()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== BLOCK STORAGE: Volumes group backups${COLOR_NORMAL}"
-  echo
-  oci --profile $lp bv volume-group-backup list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== BLOCK STORAGE: Volumes group backups${COLOR_NORMAL}"
+  oci --profile $lp bv volume-group-backup list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_object_storage_buckets()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== OBJECT STORAGE: Buckets${COLOR_NORMAL}"
-  echo
-  oci --profile $lp os bucket list -c $COMPID --output table --all --query "data [*].{Name:\"name\"}"
+  echo -e "${COLOR_TITLE2}========== OBJECT STORAGE: Buckets${COLOR_NORMAL}"
+  oci --profile $lp os bucket list -c $COMPID --output table --all --query 'data [*].{Name:name}'
 }
 
 list_file_storage_filesystems()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== FILE STORAGE: Filesystems ${COLOR_NORMAL}"
-  echo
+  echo -e "${COLOR_TITLE2}========== FILE STORAGE: Filesystems ${COLOR_NORMAL}"
   for ad in $ADS
   do
-    echo -e "${COLOR_AD}Availability-domain $ad${COLOR_NORMAL}"
-    oci --profile $lp fs file-system list -c $COMPID --output table --all --availability-domain $ad --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+    echo -e "${COLOR_AD}== Availability-domain $ad${COLOR_NORMAL}"
+    oci --profile $lp fs file-system list -c $COMPID --output table --all --availability-domain $ad --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
   done
 }
 
 list_file_storage_mount_targets()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== FILE STORAGE: Mount targets ${COLOR_NORMAL}"
-  echo
+  echo -e "${COLOR_TITLE2}========== FILE STORAGE: Mount targets ${COLOR_NORMAL}"
   for ad in $ADS
   do
-    echo -e "${COLOR_AD}Availability-domain $ad${COLOR_NORMAL}"
-    oci --profile $lp fs mount-target list -c $COMPID --output table --all --availability-domain $ad --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+    echo -e "${COLOR_AD}== Availability-domain $ad${COLOR_NORMAL}"
+    oci --profile $lp fs mount-target list -c $COMPID --output table --all --availability-domain $ad --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
   done
 }
 
 list_networking_vcns()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== NETWORKING: Virtal Cloud Networks (VCNs)${COLOR_NORMAL}"
-  echo
-  oci --profile $lp network vcn list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== NETWORKING: Virtal Cloud Networks (VCNs)${COLOR_NORMAL}"
+  oci --profile $lp network vcn list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_networking_drgs()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== NETWORKING: Dynamic Routing Gateways (DRGs)${COLOR_NORMAL}"
-  echo
-  oci --profile $lp network drg list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== NETWORKING: Dynamic Routing Gateways (DRGs)${COLOR_NORMAL}"
+  oci --profile $lp network drg list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_networking_cpes()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== NETWORKING: Customer Premises Equipments (CPEs)${COLOR_NORMAL}"
-  echo
+  echo -e "${COLOR_TITLE2}========== NETWORKING: Customer Premises Equipments (CPEs)${COLOR_NORMAL}"
   oci --profile $lp network cpe list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id}"
 }
 
 list_networking_ipsecs()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== NETWORKING: IPsec connections${COLOR_NORMAL}"
-  echo
-  oci --profile $lp network ip-sec-connection list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== NETWORKING: IPsec connections${COLOR_NORMAL}"
+  oci --profile $lp network ip-sec-connection list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_networking_lbs()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== NETWORKING: Load balancers${COLOR_NORMAL}"
-  echo
-  oci --profile $lp lb load-balancer list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== NETWORKING: Load balancers${COLOR_NORMAL}"
+  oci --profile $lp lb load-balancer list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_networking_public_ips()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== NETWORKING: Public IPs${COLOR_NORMAL}"
-  echo
-  oci --profile $lp network public-ip list -c $COMPID --scope region --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== NETWORKING: Public IPs${COLOR_NORMAL}"
+  oci --profile $lp network public-ip list -c $COMPID --scope region --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_database_db_systems()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== DATABASE: DB Systems${COLOR_NORMAL}"
-  echo
-  oci --profile $lp db system list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== DATABASE: DB Systems${COLOR_NORMAL}"
+  oci --profile $lp db system list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_database_db_systems_backups()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== DATABASE: DB Systems backups${COLOR_NORMAL}"
-  echo
-  oci --profile $lp db backup list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== DATABASE: DB Systems backups${COLOR_NORMAL}"
+  oci --profile $lp db backup list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_database_autonomous_db()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== DATABASE: Autonomous databases (ATP/ADW)${COLOR_NORMAL}"
-  echo
-  oci --profile $lp db autonomous-database list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== DATABASE: Autonomous databases (ATP/ADW)${COLOR_NORMAL}"
+  oci --profile $lp db autonomous-database list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_database_autonomous_backups()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== DATABASE: Autonomous databases backups${COLOR_NORMAL}"
-  echo
-  oci --profile $lp db autonomous-database-backup list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== DATABASE: Autonomous databases backups${COLOR_NORMAL}"
+  oci --profile $lp db autonomous-database-backup list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_resource_manager_stacks()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== RESOURCE MANAGER: Stacks${COLOR_NORMAL}"
-  echo
-  oci --profile $lp resource-manager stack list -c $COMPID --output table --all --query "data [*].{Name:\"display-name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== RESOURCE MANAGER: Stacks${COLOR_NORMAL}"
+  oci --profile $lp resource-manager stack list -c $COMPID --output table --all --query 'data [*].{Name:"display-name", OCID:id, Status:"lifecycle-state"}'
 }
 
 list_edge_services_dns_zones()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== EDGE SERVICES: DNS zones${COLOR_NORMAL}"
-  echo
-  oci --profile $lp dns zone list -c $COMPID --output table --all --query "data [*].{Name:\"name\", OCID:id, Status:\"lifecycle-state\"}" 2>/dev/null
+  echo -e "${COLOR_TITLE2}========== EDGE SERVICES: DNS zones${COLOR_NORMAL}"
+  oci --profile $lp dns zone list -c $COMPID --output table --all --query 'data [*].{Name:name, OCID:id, Status:"lifecycle-state"}' 2>/dev/null
   # 2>/dev/null needed to remove message "Query returned empty result, no output to show."
 }
 
 list_developer_services_oke()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== DEVELOPER SERVICES: Container clusters (OKE)${COLOR_NORMAL}"
-  echo
-  oci --profile $lp ce cluster list -c $COMPID --output table --all --query "data [*].{Name:\"name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== DEVELOPER SERVICES: Container clusters (OKE)${COLOR_NORMAL}"
+  oci --profile $lp ce cluster list -c $COMPID --output table --all --query 'data [*].{Name:name, OCID:id, Status:"lifecycle-state"}'
 }
 
 list_identity_policies()
 {
   local lp=$1
-  echo
-  echo -e "${COLOR_TITLE}========== IDENTITY: Policies${COLOR_NORMAL}"
-  echo
-  oci --profile $lp iam policy list -c $COMPID --output table --all --query "data [*].{Name:\"name\", OCID:id, Status:\"lifecycle-state\"}"
+  echo -e "${COLOR_TITLE2}========== IDENTITY: Policies${COLOR_NORMAL}"
+  oci --profile $lp iam policy list -c $COMPID --output table --all --query 'data [*].{Name:name, OCID:id, Status:"lifecycle-state"}'
 }
 
 list_all_objects()
@@ -345,11 +297,9 @@ list_all_objects()
   ADS=`oci --profile $lprofile iam availability-domain list|jq '.data[].name'|sed 's#"##g'`
 
   echo
-  echo -e "${COLOR_TITLE}==================== OCI Objects list for compartment ${COLOR_COMP}${lcptname}"
-  echo -e "${COLOR_TITLE}====================     (${COLOR_COMP}${lcptid}${COLOR_TITLE})"
-  echo -e "${COLOR_TITLE}==================== in region ${COLOR_COMP}${lregion}${COLOR_NORMAL}"
-
-  echo
+  echo -e "${COLOR_TITLE1}==================== OCI Objects list for compartment ${COLOR_COMP}${lcptname}"
+  echo -e "${COLOR_TITLE1}====================     (${COLOR_COMP}${lcptid}${COLOR_TITLE1})"
+  echo -e "${COLOR_TITLE1}==================== in region ${COLOR_COMP}${lregion}${COLOR_NORMAL}"
 
   list_compute_instances $lprofile
   list_compute_custom_images $lprofile
@@ -383,7 +333,7 @@ list_all_objects()
     rm -f ${OCI_CONFIG_FILE_BACKUP}
   fi
 
-  echo -e "${COLOR_TITLE}==================== End of OBJECTS LIST in region ${COLOR_COMP}${lregion}${COLOR_NORMAL}"
+  echo -e "${COLOR_TITLE1}==================== End of OBJECTS LIST in region ${COLOR_COMP}${lregion}${COLOR_NORMAL}"
 }
 
 # ---------------- misc
@@ -513,7 +463,7 @@ then
   list_all_objects $CURRENT_REGION $COMPNAME $COMPID
 else
   REGIONS_LIST=`get_all_active_regions`
-  echo -e "${COLOR_TITLE}==================== List of active regions in tenancy${COLOR_NORMAL}"
+  echo -e "${COLOR_TITLE1}==================== List of active regions in tenancy${COLOR_NORMAL}"
   for region in $REGIONS_LIST; do echo $region; done
 
   for region in $REGIONS_LIST
