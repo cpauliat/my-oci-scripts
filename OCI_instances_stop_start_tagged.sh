2133a36e6a01055f0049b4ae2534945689c3c564
cpauliat <christophe.pauliat@oracle.com>
2019-10-14 10:10:25 +0200

    update

--- OCI_instances_stop_start_tagged.sh
+++ OCI_instances_stop_start_tagged.sh
@@ -29,10 +29,11 @@ TAG_VALUE="on"
 usage()
 {
 cat << EOF
-Usage: $0 [-a] OCI_PROFILE start|stop [--confirm]
+Usage: $0 [-q] [-a] OCI_PROFILE start|stop [--confirm]
 
 Notes: 
 - OCI_PROFILE must exist in ~/.oci/config file (see example below)
+- If -q is provided, output is minimal (quiet mode): only stopped/started instances are displayed.
 - If -a is provided, the script processes all active regions instead of singe region provided in profile
 - If --confirm is not provided, the instances to stop (or start) are listed but not actually stopped (or started)
 
@@ -49,14 +50,15 @@ EOF
 process_compartment()
 {
   local lcompid=$1
-  local lregion=$2
+  local lcompname=$2
+  local lregion=$3
 
   CHANGED_FLAG=${TMP_FILE}_changed 
   rm -f $CHANGED_FLAG
 
   ${OCI} --profile $PROFILE compute instance list -c $lcompid --region $lregion --output table --query "data [*].{InstanceName:\"display-name\", InstanceOCID:id, Status:\"lifecycle-state\"}" > $TMP_FILE
-  cat $TMP_FILE
-
+  if [ $QUIET_MODE == false ]; then cat $TMP_FILE; fi
+  
   # if no instance found in this compartment (TMP_FILE empty), exit the function
   if [ ! -s $TMP_FILE ]; then rm -f $TMP_FILE; return; fi 
 
@@ -68,21 +70,22 @@ process_compartment()
       ltag_value=`${OCI} --profile $PROFILE compute instance get --region $lregion --instance-id $inst_id | jq -r '.[]."defined-tags"."osc"."stop_non_working_hours"' 2>/dev/null`
       if [ "$ltag_value" == "$TAG_VALUE" ]
       then 
+        if [ $QUIET_MODE == true ]; then printf "region $lregion, cpt $lcompname: "; else printf " --> "; fi             
         if [ $CONFIRM == true ]
         then
           case $ACTION in
-            "start") echo "--> STARTING instance $inst_name ($inst_id) because of TAG VALUE"
+            "start") echo "STARTING instance $inst_name ($inst_id)"
                      ${OCI} --profile $PROFILE compute instance action --region $lregion --instance-id $inst_id --action START >/dev/null 2>&1
                      ;;
-            "stop")  echo "--> STOPPING instance $inst_name ($inst_id) because of TAG VALUE"
+            "stop")  echo "STOPPING instance $inst_name ($inst_id)"
                      ${OCI} --profile $PROFILE compute instance action --region $lregion --instance-id $inst_id --action SOFTSTOP >/dev/null 2>&1
                      ;;
           esac
           touch $CHANGED_FLAG
         else
           case $ACTION in
-            "start")  echo "--> Instance $inst_name ($inst_id) SHOULD BE STARTED because of TAG VALUE --> re-run script with --confirm to actually start instances"  ;;
-            "stop")   echo "--> Instance $inst_name ($inst_id) SHOULD BE STOPPED because of TAG VALUE --> re-run script with --confirm to actually stop instances"  ;;
+            "start")  echo "Instance $inst_name ($inst_id) SHOULD BE STARTED --> re-run script with --confirm to actually start instances"  ;;
+            "stop")   echo "Instance $inst_name ($inst_id) SHOULD BE STOPPED --> re-run script with --confirm to actually stop instances"  ;;
           esac
         fi
       fi
@@ -91,8 +94,10 @@ process_compartment()
 
   if [ -f $CHANGED_FLAG ]
   then
-    ${OCI} --profile $PROFILE compute instance list -c $lcompid --region $lregion --output table --query "data [*].{InstanceName:\"display-name\", InstanceOCID:id, Status:\"lifecycle-state\"}" 
     rm -f $CHANGED_FLAG
+    if [ $QUIET_MODE == false ]; then 
+      ${OCI} --profile $PROFILE compute instance list -c $lcompid --region $lregion --output table --query "data [*].{InstanceName:\"display-name\", InstanceOCID:id, Status:\"lifecycle-state\"}" 
+    fi
   fi
   
   rm -f $TMP_FILE
@@ -119,7 +124,9 @@ OCI=$HOME/bin/oci
 
 ALL_REGIONS=false
 CONFIRM=false
+QUIET_MODE=false
 
+if  [ "$1" == "-q" ]; then QUIET_MODE=true; shift; fi
 if  [ "$1" == "-a" ]; then ALL_REGIONS=true; shift; fi
 
 case $# in 
@@ -164,20 +171,29 @@ fi
 # -- process required regions list
 for region in $REGIONS_LIST
 do
-  echo -e "==================== REGION ${region}"
+  if [ $QUIET_MODE == false ]
+  then
+    echo -e "==================== REGION ${region}"
+  fi
 
   # -- list instances in the root compartment
-  echo
-  echo "Compartment root, OCID=$TENANCYOCID"
-  process_compartment $TENANCYOCID $region
+  if [ $QUIET_MODE == false ]
+  then
+    echo
+    echo "Compartment root, OCID=$TENANCYOCID"
+  fi
+  process_compartment $TENANCYOCID root $region
 
   # -- list instances compartment by compartment (excluding root compartment but including all subcompartments). Only ACTIVE compartments
   ${OCI} --profile $PROFILE iam compartment list -c $TENANCYOCID --compartment-id-in-subtree true --all --query "data [?\"lifecycle-state\" == 'ACTIVE']" 2>/dev/null| egrep "^ *\"name|^ *\"id"|awk -F'"' '{ print $4 }' | while read compid
   do
     read compname
-    echo
-    echo "Compartment $compname, OCID=$compid"
-    process_compartment $compid $region
+    if [ $QUIET_MODE == false ]
+    then
+      echo
+      echo "Compartment $compname, OCID=$compid"
+    fi
+    process_compartment $compid $compname $region
   done
 done
 
