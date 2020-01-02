5880a4a33682ef2a2cd19f30e9c539915edf52c1
cpauliat <christophe.pauliat@oracle.com>
2020-01-02 14:27:13 +0100

    update

--- OCI_generate_api_keys.sh
+++ OCI_generate_api_keys.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# generate OCI API key pair
+# This Bash script generates OCI API key pair as described in documentation
+# on https://docs.cloud.oracle.com/iaas/Content/API/Concepts/apisigningkey.htm
+# it creates 3 files (private key, public key and fingerprint) in the current directory
+#
+# Author: christophe.pauliat@oracle.com
+# Last update: January 2, 2020
+
 if [ $# -ne 1 ]; then echo "Usage: $0 key_name"; exit 1; fi
 
 myname=$1
 
-# ---- API key pair (will create files apikey.pem and apikey_public.pem)
-# ---- see doc on https://docs.cloud.oracle.com/iaas/Content/API/Concepts/apisigningkey.htm
 openssl genrsa -out ./apikey_${myname}.pem 2048
 chmod 600 ./apikey_${myname}.pem
 openssl rsa -pubout -in ./apikey_${myname}.pem -out ./apikey_${myname}_public.pem 
