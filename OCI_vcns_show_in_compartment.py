648f5875590f5ec5bfbf46ae8947ea53802332e9
cpauliat <christophe.pauliat@oracle.com>
2020-02-28 16:10:34 +0100

    update

--- OCI_vcns_show_in_compartment.py
+++ OCI_vcns_show_in_compartment.py
@@ -54,10 +54,12 @@ configfile = "~/.oci/config"    # Define config file to be used.
 
 # -- usage syntax
 def usage():
-    print ("Usage: {} OCI_PROFILE compartment_ocid".format(sys.argv[0]))
-    print ("    or {} OCI_PROFILE compartment_name".format(sys.argv[0]))  
+    print ("Usage: {} [-i] OCI_PROFILE compartment_ocid".format(sys.argv[0]))
+    print ("    or {} [-i] OCI_PROFILE compartment_name".format(sys.argv[0]))  
     print ("")
-    print ("note: OCI_PROFILE must exist in {} file (see example below)".format(configfile))
+    print ("Notes:")
+    print ("- If -i is provided, then OCIDs of objects are also displayed")
+    print ("- OCI_PROFILE must exist in {} file (see example below)".format(configfile))
     print ("")
     print ("[EMEAOSCf]")
     print ("tenancy     = ocid1.tenancy.oc1..aaaaaaaaw7e6nkszrry6d5hxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
@@ -111,16 +113,22 @@ def list_vcns (cpt_ocid,cpt_name):
     response_vcn = oci.pagination.list_call_get_all_results(VirtualNetworkClient.list_vcns,compartment_id=cpt_ocid)
     if len(response_vcn.data) > 0:
         for vcn in response_vcn.data:
+
+            # -- VCN name, CIDR, dns label
             print ("")
             print (COLOR_LYELLOW+"---------------------------------------- VCN = ",end='')
             print (COLOR_LRED+'{:s}'.format(vcn.display_name)+COLOR_NORMAL,end='')
             print (COLOR_LBLUE+' {:s}'.format(vcn.cidr_block)+COLOR_NORMAL,end='')
             try:
-                print (COLOR_NORMAL+' {:s}.oraclevcn.com'.format(vcn.dns_label)+COLOR_NORMAL,end='')
+                print (COLOR_LYELLOW+' {:s}.oraclevcn.com'.format(vcn.dns_label)+COLOR_NORMAL,end='')
             except:
-                print (COLOR_NORMAL+' <no DNS label>'+COLOR_NORMAL,end='')
-            print ("")
-            #print (COLOR_NORMAL+' ({:s})'.format(vcn.id))
+                print (COLOR_LYELLOW+' <no DNS label>'+COLOR_NORMAL,end='')
+            if display_ocid:
+                print (COLOR_NORMAL+' ({:s})'.format(vcn.id))
+            else:
+                print ("")
+
+            # -- subnets
             response_subnet = oci.pagination.list_call_get_all_results(VirtualNetworkClient.list_subnets,compartment_id=cpt_ocid,vcn_id=vcn.id)
             if len(response_subnet.data) > 0:
                 for subnet in response_subnet.data:
@@ -128,22 +136,32 @@ def list_vcns (cpt_ocid,cpt_name):
                     print ('    subnet = '+COLOR_GREEN+'{:s}'.format(subnet.display_name),end='')
                     print (COLOR_LBLUE+' {:s}'.format(subnet.cidr_block)+COLOR_NORMAL,end='')
                     try:
-                        print (COLOR_NORMAL+' {:s}.{:s}.oraclevcn.com'.format(subnet.dns_label,vcn.dns_label)+COLOR_NORMAL,end='')
+                        print (COLOR_LYELLOW+' {:s}.{:s}.oraclevcn.com'.format(subnet.dns_label,vcn.dns_label)+COLOR_NORMAL,end='')
                     except:
-                        print (COLOR_NORMAL+' <no DNS label>'+COLOR_NORMAL,end='')
-                    print ("")
-                    #print (COLOR_NORMAL+' ({:s})'.format(subnet.id))
+                        print (COLOR_LYELLOW+' <no DNS label>'+COLOR_NORMAL,end='')
+                    if display_ocid:
+                        print (COLOR_NORMAL+' ({:s})'.format(subnet.id))
+                    else:
+                        print ("")
 
                     # route table and route rules
                     response_rt=VirtualNetworkClient.get_route_table(subnet.route_table_id)
-                    print ("        route table   = "+COLOR_LMAGENTA+"{}".format(response_rt.data.display_name)+COLOR_NORMAL)
+                    print ("        route table   = "+COLOR_LMAGENTA+"{}".format(response_rt.data.display_name)+COLOR_NORMAL,end='')
+                    if display_ocid:
+                        print (COLOR_NORMAL+' ({:s})'.format(response_rt.data.id))
+                    else:
+                        print ("")
                     for rule in response_rt.data.route_rules:
                         print (COLOR_CYAN+"            {:18s} --> {:s}".format(rule.destination,rule.network_entity_id)+COLOR_NORMAL)
 
                     # security lists and security rules
                     for sl_id in subnet.security_list_ids:
                         response_sl=VirtualNetworkClient.get_security_list(sl_id)
-                        print ("        security list = "+COLOR_LMAGENTA+"{}".format(response_sl.data.display_name)+COLOR_NORMAL)
+                        print ("        security list = "+COLOR_LMAGENTA+"{}".format(response_sl.data.display_name)+COLOR_NORMAL,end='')
+                        if display_ocid:
+                            print (COLOR_NORMAL+' ({:s})'.format(response_sl.data.id))
+                        else:
+                            print ("")                       
                         print ("            ingress:")
                         for rule in response_sl.data.ingress_security_rules:
                             print (COLOR_CYAN+"                source       {:18s} {:s}".format(rule.source,rule_details(rule))+COLOR_NORMAL)
@@ -151,18 +169,48 @@ def list_vcns (cpt_ocid,cpt_name):
                         for rule in response_sl.data.egress_security_rules:
                             print (COLOR_CYAN+"                destination  {:18s} {:4s}".format(rule.destination,rule_details(rule))+COLOR_NORMAL)
 
+            # -- network security group
+            response_nsg = oci.pagination.list_call_get_all_results(VirtualNetworkClient.list_network_security_groups,compartment_id=cpt_ocid,vcn_id=vcn.id)
+            if len(response_subnet.data) > 0:
+                for nsg in response_nsg.data:
+                    # nsg name, nsg id
+                    print ('    network security group = '+COLOR_LMAGENTA+'{:s}'.format(nsg.display_name)+COLOR_NORMAL,end='')
+                    if display_ocid:
+                        print (' ({:s})'.format(nsg.id))
+                    else:
+                        print ("")
+
+                    # security rules
+                    response_sr=VirtualNetworkClient.list_network_security_group_security_rules(nsg.id)
+                    print ("            ingress:")
+                    for rule in response_sr.data:
+                        if rule.direction == "INGRESS":
+                            print (COLOR_CYAN+"                source       {:18s} {:s}".format(rule.source,rule_details(rule))+COLOR_NORMAL)
+                    print ("            egress:")
+                    for rule in response_sr.data:
+                        if rule.direction == "EGRESS":
+                            print (COLOR_CYAN+"                destination  {:18s} {:s}".format(rule.destination,rule_details(rule))+COLOR_NORMAL)
 
 # ------------ main
 global config
 global IdentityClient
 global initial_cpt_ocid
 global initial_cpt_name
+global display_ocid
 
 # -- parse arguments
 
 if len(sys.argv) == 3:
+    display_ocid = False
     profile  = sys.argv[1] 
     cpt      = sys.argv[2]
+elif len(sys.argv) == 4:
+    if sys.argv[1] == "-i":
+        display_ocid = True
+    else:
+        usage()
+    profile  = sys.argv[2] 
+    cpt      = sys.argv[3]
 else:
     usage()
 
