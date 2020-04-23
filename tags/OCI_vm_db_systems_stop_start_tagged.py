dec194d3514f25671fa43c33ba2bab3413cc02e8
cpauliat <christophe.pauliat@oracle.com>
2020-04-23 09:57:03 +0200

    update

--- tags/OCI_vm_db_systems_stop_start_tagged.py
+++ tags/OCI_vm_db_systems_stop_start_tagged.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 # ---------------------------------------------------------------------------------------------------------------------------------
-# This script looks for autonomous databases with a specific tag key and stop (or start) them if the 
+# This script looks for VM database systems with a specific tag key and stop (or start) them if the 
 #     tag value for the tag key matches the current time.
-# You can use it to automatically stop some autonomous databases during non working hours
+# You can use it to automatically stop some VM database systems during non working hours
 #     and start them again at the beginning of working hours to save cloud credits
 # This script needs to be executed every hour during working days by an external scheduler 
 #     (cron table on Linux for example)
-# You can add the 2 tag keys to the default tags for root compartment so that every new autonomous 
-#     database get those 2 tag keys with default value ("off" or a specific UTC time)
+# You can add the 2 tag keys to the default tags for root compartment so that every new VM database system 
+#     get those 2 tag keys with default value ("off" or a specific UTC time)
 #
 # This script looks in all compartments in a OCI tenant in a region using OCI Python SDK
 # Note: OCI tenant and region given by an OCI CLI PROFILE
@@ -29,7 +29,7 @@ import os
 from datetime import datetime
 
 # ---------- Tag names, key and value to look for
-# Autonomous DBs tagged using this will be stopped/started.
+# VM DB systems tagged using this will be stopped/started.
 # Update these to match your tags.
 tag_ns        = "osc"
 tag_key_stop  = "automatic_shutdown"
@@ -46,8 +46,8 @@ def usage():
     print ("")
     print ("Notes:")
     print ("    If -a is provided, the script processes all active regions instead of singe region provided in profile")
-    print ("    If --confirm_stop  is not provided, the autonomous databases to stop are listed but not actually stopped")
-    print ("    If --confirm_start is not provided, the autonomous databases to start are listed but not actually started")
+    print ("    If --confirm_stop  is not provided, the VM database systems to stop are listed but not actually stopped")
+    print ("    If --confirm_start is not provided, the VM database systems to start are listed but not actually started")
     print ("")
     print ("note: OCI_PROFILE must exist in {} file (see example below)".format(configfile))
     print ("")
@@ -59,7 +59,7 @@ def usage():
     print ("region      = eu-frankfurt-1")
     exit (1)
 
-# ---- Check autonomous databases in a compartment
+# ---- Check VM database systems in a compartment
 def process_compartment(lcpt):
     global config
     global current_utc_time
@@ -71,38 +71,42 @@ def process_compartment(lcpt):
     # region 
     region = config["region"]
 
-    # find autonomous databases in this compartment
-    response = oci.pagination.list_call_get_all_results(DatabaseClient.list_autonomous_databases,compartment_id=lcpt.id)
+    # find VM database systems in this compartment
+    response = oci.pagination.list_call_get_all_results(DatabaseClient.list_db_systems,compartment_id=lcpt.id)
  
     # for each instance, check if it needs to be stopped or started 
     if len(response.data) > 0:
-        for adb in response.data:
-            if adb.lifecycle_state != "TERMINED":
+        for dbs in response.data:
+            if dbs.lifecycle_state != "TERMINED":
                 # get the tags
                 try:
-                    tag_value_stop  = adb.defined_tags[tag_ns][tag_key_stop]
-                    tag_value_start = adb.defined_tags[tag_ns][tag_key_start]
+                    tag_value_stop  = dbs.defined_tags[tag_ns][tag_key_stop]
+                    tag_value_start = dbs.defined_tags[tag_ns][tag_key_start]
                 except:
                     tag_value_stop  = "none"
                     tag_value_start = "none"
                 
+                # get the DB node 
+                response = DatabaseClient.list_db_nodes(compartment_id=lcpt.id, db_system_id=dbs.id)
+                dbnode = response.data[0]
+
                 # Is it time to start this autonomous db ?
-                if adb.lifecycle_state == "STOPPED" and tag_value_start == current_utc_time:
+                if dbs.lifecycle_state == "STOPPED" and tag_value_start == current_utc_time:
                     print ("{:s}, {:s}, {:s}: ".format(datetime.utcnow().strftime("%T"), region, lcpt.name),end='')
                     if confirm_start:
-                        print ("STARTING autonomous db {:s} ({:s})".format(adb.display_name, adb.id))
-                        DatabaseClient.start_autonomous_database(adb.id)
+                        print ("STARTING DB node for {:s} ({:s})".format(dbs.display_name, dbs.id))
+                        DatabaseClient.db_node_action(dbnode.id, "START")
                     else:
-                        print ("Autonomous DB {:s} ({:s}) SHOULD BE STARTED --> re-run script with --confirm_start to actually start databases".format(adb.display_name, adb.id))
+                        print ("DB node for DB system {:s} ({:s}) SHOULD BE STARTED --> re-run script with --confirm_start to actually start databases".format(dbs.display_name, dbs.id))
 
                 # Is it time to stop this autonomous db ?
-                elif adb.lifecycle_state == "RUNNING" and tag_value_stop == current_utc_time:
+                elif dbs.lifecycle_state == "RUNNING" and tag_value_stop == current_utc_time:
                     print ("{:s}, {:s}, {:s}: ".format(datetime.utcnow().strftime("%T"), region, lcpt.name),end='')
                     if confirm_stop:
-                        print ("STOPPING autonomous db {:s} ({:s})".format(adb.display_name, adb.id))
-                        DatabaseClient.stop_autonomous_database(adb.id)
+                        print ("STOPPING DB node for {:s} ({:s})".format(dbs.display_name, dbs.id))
+                        DatabaseClient.db_node_action(dbnode.id, "STOP")
                     else:
-                        print ("Autonomous DB {:s} ({:s}) SHOULD BE STOPPED --> re-run script with --confirm_start to actually stop databases".format(adb.display_name, adb.id))
+                        print ("DB node for DB system {:s} ({:s}) SHOULD BE STOPPED --> re-run script with --confirm_start to actually stop databases".format(dbs.display_name, dbs.id))
 
   
 # ------------ main
