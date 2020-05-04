29b291d18c94ba7a1285e304819e16a1be39b2dd
cpauliat <christophe.pauliat@oracle.com>
2020-05-04 10:52:08 +0200

    update

--- tags/OCI_object_add_tag.py
+++ tags/OCI_object_add_tag.py
@@ -16,6 +16,7 @@
 # Versions
 #    2020-04-27: Initial Version
 #    2020-05-04: fixed bug if tag namespace not already used for this object
+#    2020-05-04: Simplify code
 #
 # TO DO: add support for more resource types
 # --------------------------------------------------------------------------------------------
@@ -54,69 +55,34 @@ def update_tags(ltags):
     return (ltags)
 
 def add_tag_compute_instance(inst_id):
-    global config
-
-    # Get Defined-tags for the compute instance
     ComputeClient = oci.core.ComputeClient(config)
     try:
         response = ComputeClient.get_instance(inst_id)
-        instance = response.data
-    except:
-        print ("ERROR 03: instance with OCID '{}' not found !".format(inst_id))
-        exit (3)
-
-    # Add tag key to tag namespace and update compute instance
-    tags = update_tags(instance.defined_tags)
-    try:
+        tags = update_tags(response.data.defined_tags)
         ComputeClient.update_instance(inst_id, oci.core.models.UpdateInstanceDetails(defined_tags=tags))
     except:
-        print ("ERROR 05: cannot add this tag key with this tag value !")
         print (sys.exc_info()[1].message)
-        exit (5)
+        exit (3)
 
 def add_tag_db_system(dbs_id):
-    global config
-
-    # Get Defined-tags for the db system
     DatabaseClient = oci.database.DatabaseClient(config)
-
     try:
         response = DatabaseClient.get_db_system(dbs_id)
-        dbs = response.data
-    except:
-        print ("ERROR 03: db system with OCID '{}' not found !".format(dbs_id))
-        exit (3)
-
-    # Add tag key to tag namespace and update compute instance
-    tags = update_tags(dbs.defined_tags)
-    try:
+        tags = update_tags(response.data.defined_tags)
         DatabaseClient.update_db_system(dbs_id, oci.database.models.UpdateDbSystemDetails(defined_tags=tags))
     except:
-        print ("ERROR 05: cannot add this tag key with this tag value !")
         print (sys.exc_info()[1].message)
-        exit (5)
-
-def add_tag_autonomous_db(adb_id):
-    global config
-
-    # Get Defined-tags for the autonomous DB
-    DatabaseClient = oci.database.DatabaseClient(config)
-
-    try:
-        response = DatabaseClient.get_autonomous_database(adb_id)
-        adb = response.data
-    except:
-        print ("ERROR 03: Autonomous DB with OCID '{}' not found !".format(adb_id))
         exit (3)
 
-    # Add tag key to tag namespace and update autonomous DB
-    tags = update_tags(adb.defined_tags)
+def add_tag_autonomous_db(adb_id):
+    DatabaseClient = oci.database.DatabaseClient(config)
     try:
+        response = DatabaseClient.get_db_system(dbs_id)
+        tags = update_tags(response.data.defined_tags)
         DatabaseClient.update_autonomous_database(adb_id, oci.database.models.UpdateAutonomousDatabaseDetails(defined_tags=tags))
     except:
-        print ("ERROR 05: cannot add this tag key with this tag value !")
         print (sys.exc_info()[1].message)
-        exit (5)
+        exit (3)
 
 # ------------ main
 
