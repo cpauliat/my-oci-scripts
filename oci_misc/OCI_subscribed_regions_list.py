9152e4b92e55e0ae89f2cc94627abb0ec5938372
christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2021-02-25 16:11:41 +0100

    update

---
+++ oci_misc/OCI_subscribed_regions_list.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+# ---------------------------------------------------------------------------------------------------------------------------------
+# This script lists the subscribed regions using OCI Python SDK
+#
+# Note: OCI tenant given by an OCI CLI PROFILE
+# Author        : Christophe Pauliat
+# Platforms     : MacOS / Linux
+#
+# prerequisites : - Python 3 with OCI Python SDK installed
+#                 - OCI config file configured with profiles
+# Versions
+#    2021-02-25: Initial Version
+# ---------------------------------------------------------------------------------------------------------------------------------
+
+# -- import
+import oci
+import sys
+
+# ---------- Functions
+
+# ---- variables
+configfile = "~/.oci/config"    # Define config file to be used.
+
+# ---- usage syntax
+def usage():
+    print ("Usage: {} OCI_PROFILE".format(sys.argv[0]))
+    print ("")
+    print ("note: OCI_PROFILE must exist in {} file (see example below)".format(configfile))
+    print ("")
+    print ("[EMEAOSCf]")
+    print ("tenancy     = ocid1.tenancy.oc1..aaaaaaaaw7e6nkszrry6d5hxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print ("user        = ocid1.user.oc1..aaaaaaaayblfepjieoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print ("fingerprint = 19:1d:7b:3a:17:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx")
+    print ("key_file    = /Users/cpauliat/.oci/api_key.pem")
+    print ("region      = eu-frankfurt-1")
+    exit (1)
+
+# ------------ main
+
+# -- parse arguments
+
+if len(sys.argv) == 2:
+    profile  = sys.argv[1] 
+else:
+    usage()
+
+# -- load profile from config file
+try:
+    config = oci.config.from_file(configfile,profile)
+
+except:
+    print ("ERROR 02: profile '{}' not found in config file {} !".format(profile,configfile))
+    exit (2)
+
+IdentityClient = oci.identity.IdentityClient(config)
+user = IdentityClient.get_user(config["user"]).data
+RootCompartmentID = user.compartment_id
+
+# -- get list of subscribed regions
+response = oci.pagination.list_call_get_all_results(IdentityClient.list_region_subscriptions, RootCompartmentID)
+regions = response.data
+
+for region in regions:
+    print (region.region_name)
+
+
+# -- the end
+exit (0)
